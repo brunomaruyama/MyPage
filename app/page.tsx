fea21dc60c6dae4353e0b7ebbc2724d64a460ae3
brunomaruyama <brunoyuitiro@hotmail.com>
2024-06-29 00:38:34 -0300

    placing elements

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,26 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <header className="flex space-x-0 p-5 justify-between">
-        <h1>Bruno Y Maruyama</h1>
+      <header className="flex p-5 justify-end space-x-2">
+        <button>PT</button>
+        <button>Icon</button>
+      </header>
 
-        <ul className="flex space-x-3">
+      <div className="flex h-4/5 justify-between items-center">
+        <div className="block">
+          <Image
+            src="/../public/DSC08867.jpg"
+            width={240}
+            height={240}
+            alt="Picture of Bruno Maruyama"
+            style={{ objectFit: "contain" }}
+          />
+
+          <h1>Bruno Y Maruyama</h1>
+          <h2>Front-End Developer</h2>
+        </div>
+
+        <ul className="block">
           <li>
             <a href="http://">Sobre mim</a>
           </li>
@@ -17,7 +33,12 @@ export default function Home() {
             <a href="http://">Contato</a>
           </li>
         </ul>
-      </header>
+      </div>
+
+      <footer className="flex space-x-2 fixed bottom-0">
+        <a href="">github</a>
+        <a href="">linkedin</a>
+      </footer>
     </main>
   );
 }
