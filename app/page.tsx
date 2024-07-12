7fa39f2c23905881a0c279a592ef7e15c62c7304
brunomaruyama <brunoyuitiro@hotmail.com>
2024-07-11 21:15:13 -0300

    hovers, dynamic light button and layouts

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <main className="h-[100vh] grid">
+      <main className="h-[100dvh]">
         <Header />
-        <div className="row-span-9 sm:px-[5vw] lg:px-[10vw] my-[auto]">
+        <div className="h-[100vh] w-full flex flex-wrap justify-between items-center  sm:items-center sm:px-[5vw] lg:px-[10vw] my-[auto] px-3 py-8">
           <div className="block space-y-3 ">
             <Image
               src="/DSC08867.jpg"
@@ -25,18 +25,19 @@ export default function Home() {
               MARUYAMA
             </h1>
             <h2 className="text-2xl font-light leading-none">
-              Desenvolvedor Front-End
+              Front-End Developer
             </h2>
           </div>
-          <ul className="text-4xl sm:text-right text-left pt-6 font-bold">
-            <li className="hover:text-blue-100 duration-500 hover:translate-x-2">
-              <a href="http://">Sobre mim</a>
+
+          <ul className="text-4xl sm:text-right text-left sm:pt-6 font-bold flex-col">
+            <li className="hover:text-orange-300 duration-500 hover:translate-x-2">
+              <a href="http://">About</a>
             </li>
-            <li className="hover:text-blue-100 duration-500 hover:translate-x-2">
-              <a href="http://">Projetos</a>
+            <li className="hover:text-orange-300 duration-500 hover:translate-x-2">
+              <a href="http://">Projects</a>
             </li>
-            <li className="hover:text-blue-100 duration-500 hover:translate-x-2">
-              <a href="http://">Contato</a>
+            <li className="hover:text-orange-300 duration-500 hover:translate-x-2">
+              <a href="http://">Contact</a>
             </li>
           </ul>
         </div>
