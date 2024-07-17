29fee82c439e9488f512966d287059924c990706
brunomaruyama <brunoyuitiro@hotmail.com>
2024-07-16 23:01:59 -0300

    about page, active link, texture background, layout adjustments

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/no-unescaped-entities */
 
 import Links from "../../components/Links/page";
+import Image from "next/image";
 
 export default function About() {
   return (
     <div className="h-[100vh] flex justify-between space-x-10 items-center sm:px-[5vw] lg:px-[10vw] my-[auto] px-3 py-8">
-      <div>
+      <div className="w-1/2">
         <h1 className="text-4xl font-bold py-2">About me</h1>
         <p className="font-medium">
           Graduated in Biological Sciences from the University of São Paulo
@@ -16,9 +17,16 @@ export default function About() {
         </p>
 
         <h2 className="text-3xl font-bold py-2 mt-8">
-          Technologies I've been learning
+          Technologies I've been using in my projects
         </h2>
-        <div></div>
+        <div className="flex flex-wrap justify-between">
+          <Image
+            src="next-js-svgrepo-com.svg"
+            alt="nextJS logo"
+            width={40}
+            height={40}
+          />
+        </div>
       </div>
       <Links />
     </div>
