dbda4703700f2a6c84c8696b4a471fbee9f39455
brunomaruyama <brunoyuitiro@hotmail.com>
2024-08-26 17:47:03 -0300

    small layout

--- app/page.tsx
+++ app/page.tsx
@@ -10,18 +10,18 @@ export default function Home() {
       <Header />
       <section
         id="home"
-        className="pt-[15vh] sm:h-[100vh] w-full grid md:grid-cols-3 sm:px-[5vw] lg:px-[10vw] my-[auto] px-3"
+        className="pt-[8vh] sm:pt-[15vh] sm:h-[100vh] w-full grid md:grid-cols-3 sm:px-[5vw] lg:px-[10vw] my-[auto] px-3"
       >
-        <div className="col-span-2 block space-y-3 items-end mb-8">
+        <div className="col-span-2 block space-y-3 items-end mb-10">
           <Image
             src="/DSC08868.jpg"
-            width={150}
-            height={150}
+            width={250}
+            height={250}
             quality={80}
             priority={true}
             alt="Picture of Bruno Maruyama"
             style={{ objectFit: "contain" }}
-            className="hover:shadow-purple-400/50 shadow-black/30 duration-500 shadow-2xl rounded-full w-auto h-auto"
+            className="hover:shadow-purple-400/50 shadow-black/30 duration-500 shadow-2xl rounded-full w-auto max-h-40 sm:max-h-80"
           />
           <div className="block">
             <h1 className="text-4xl sm:text-7xl leading-none font-black title">
@@ -35,26 +35,26 @@ export default function Home() {
           </div>
         </div>
         <div className="col-span-1">
-          <div className="mb-4">
-            <h2 className="text-2xl font-bold">Education</h2>
+          <div className="mb-8 title duration-1000 delay-300">
+            <h2 className="text-2xl font-black">Education</h2>
             <p className="font-light">
               - Bachelor´s and Licentiate Degree in
-              <span className="font-semibold"> Biological Sciences</span> -
+              <span className="font-medium"> Biological Sciences</span> -
               Universidade de São Paulo
             </p>
             <p className="font-light">
               - MBA Specialization Degree in
-              <span className="font-semibold"> Data Science and Analytics</span>
+              <span className="font-medium"> Data Science and Analytics</span>
             </p>
           </div>
-          <div>
-            <h2 className="text-2xl font-bold">Languages</h2>
+          <div className="title duration-1000 delay-700">
+            <h2 className="text-2xl font-black">Languages</h2>
             <p className="font-light">
-              - <span className="font-semibold">Brazilian portuguese</span>:
+              - <span className="font-medium">Brazilian portuguese</span>:
               native speaker
             </p>
             <p className="font-light">
-              - <span className="font-semibold">English</span>: advanced
+              - <span className="font-medium">English</span>: advanced
             </p>
           </div>
         </div>
