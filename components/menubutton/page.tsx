071e6a70d3f175f66890468d6ea0775b6eb14020
brunomaruyama <brunoyuitiro@hotmail.com>
2024-08-27 20:57:14 -0300

    correction on menu

--- components/menubutton/page.tsx
+++ components/menubutton/page.tsx
@@ -31,7 +31,7 @@ export default function MenuBtn() {
       <ul
         className={`${
           isOpen ? "block space-x-0 text-center" : "hidden"
-        } sm:flex  text-xl font-normal tracking-widest justify-center items-center space-x-10`}
+        } sm:flex  text-xl font-normal tracking-widest justify-center items-center sm:space-x-10`}
       >
         <li className="hover:text-purple-400  duration-500 hover:translate-y-1">
           <Link href="#about">About</Link>
